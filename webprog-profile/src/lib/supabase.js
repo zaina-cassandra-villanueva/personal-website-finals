3738277885acb117ab4073f41a455e4410b365b5
Zaina Cassandra S. Villanueva <zsvillanueva@student.apc.edu.ph>
2026-02-27 04:08:15 +0000

    ok

--- webprog-profile/src/lib/supabase.js
+++ webprog-profile/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://gsiuvidzwlqyvqsuemsv.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzaXV2aWR6d2xxeXZxc3VlbXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4Mzg3OTksImV4cCI6MjA4NDQxNDc5OX0.bjIxUbEtTTtWWc59843jKXH_Bp2fk891-yJMpZwk6Wc";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
